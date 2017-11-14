593dd946a800373a99f72315e71780823958e25e
MNjegovan <mnjegovan99@gmail.com>
2017-11-14 22:00:15 +0100

    Update pehape.php

--- pehape.php
+++ pehape.php
@@ -3,3 +3,8 @@ $ime = $_GET['ime'];
 $prezime = $_GET['prezime'];
 $ulica = $_GET['ulica'];
 $broj = $_GET['broj'];
+$primjedba = $_GET['primjedba'];
+$zemlja = $_GET['zemlja'];
+echo "Ime: ".$ime."<br>"."Prezime: ".$prezime."<br>"."Ulica: ".$ulica."<br>"."Broj: ".$broj."<br>"."Primjedba"."<br>".
+$primjedba."<br>"."Zemlja: ".$zemlja;
+?>
